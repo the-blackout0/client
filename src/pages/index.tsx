a9d564f2fb78653d9e011f137e47924d0f30e5b2
lucasmanea21 <lucasmanea@Yahoo.com>
2023-04-26 11:07:34 +0300

    add supabase connection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import ThemeSwitcher from '@/components/ThemeSwitcher'
 
 const Home: FC = () => {
 	return (
-		<div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+		<div className="relative flex justify-center min-h-screen py-4 bg-gray-100 items-top dark:bg-gray-900 sm:items-center sm:pt-0">
 			<div className="absolute top-6 right-6">
 				<ConnectWallet />
 			</div>
@@ -16,15 +16,15 @@ const Home: FC = () => {
 					<h1 className="text-6xl font-bold dark:text-white">{APP_NAME}</h1>
 				</div>
 
-				<div className="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+				<div className="mt-8 overflow-hidden bg-white shadow dark:bg-gray-800 sm:rounded-lg">
 					<div className="grid grid-cols-1 md:grid-cols-2">
 						<div className="p-6">
 							<div className="flex items-center">
 								<BookOpenIcon className="w-8 h-8 text-gray-500" />
-								<div className="ml-4 text-lg leading-7 font-semibold">
+								<div className="ml-4 text-lg font-semibold leading-7">
 									<a
 										href="https://nextjs.org/docs"
-										className="underline text-gray-900 dark:text-white"
+										className="text-gray-900 underline dark:text-white"
 									>
 										Next.js Docs
 									</a>
@@ -32,7 +32,7 @@ const Home: FC = () => {
 							</div>
 
 							<div className="ml-12">
-								<div className="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+								<div className="mt-2 text-sm text-gray-600 dark:text-gray-400">
 									Next.js gives you the best developer experience with all the features you need for
 									production: hybrid static &amp; server rendering, TypeScript support, smart
 									bundling, route pre-fetching, and more. No config needed.
@@ -43,15 +43,15 @@ const Home: FC = () => {
 						<div className="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
 							<div className="flex items-center">
 								<BookOpenIcon className="w-8 h-8 text-gray-500" />
-								<div className="ml-4 text-lg leading-7 font-semibold">
-									<a href="https://wagmi.sh" className="underline text-gray-900 dark:text-white">
+								<div className="ml-4 text-lg font-semibold leading-7">
+									<a href="https://wagmi.sh" className="text-gray-900 underline dark:text-white">
 										wagmi Docs
 									</a>
 								</div>
 							</div>
 
 							<div className="ml-12">
-								<div className="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+								<div className="mt-2 text-sm text-gray-600 dark:text-gray-400">
 									wagmi is a collection of React Hooks containing everything you need to start working
 									with Ethereum. wagmi makes it easy to display ENS and balance information, sign
 									messages, interact with contracts, and much more — all with caching, request
@@ -63,10 +63,10 @@ const Home: FC = () => {
 						<div className="p-6 border-t border-gray-200 dark:border-gray-700">
 							<div className="flex items-center">
 								<BookOpenIcon className="w-8 h-8 text-gray-500" />
-								<div className="ml-4 text-lg leading-7 font-semibold">
+								<div className="ml-4 text-lg font-semibold leading-7">
 									<a
 										href="https://laravel-news.com/"
-										className="underline text-gray-900 dark:text-white"
+										className="text-gray-900 underline dark:text-white"
 									>
 										Tailwind Docs
 									</a>
@@ -74,7 +74,7 @@ const Home: FC = () => {
 							</div>
 
 							<div className="ml-12">
-								<div className="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+								<div className="mt-2 text-sm text-gray-600 dark:text-gray-400">
 									Tailwind CSS is a highly customizable, low-level CSS framework that gives you all of
 									the building blocks you need to build bespoke designs without any annoying
 									opinionated styles you have to fight to override.
@@ -85,13 +85,13 @@ const Home: FC = () => {
 						<div className="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
 							<div className="flex items-center">
 								<CodeIcon className="w-8 h-8 text-gray-500" />
-								<div className="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">
+								<div className="ml-4 text-lg font-semibold leading-7 text-gray-900 dark:text-white">
 									About this Template
 								</div>
 							</div>
 
 							<div className="ml-12">
-								<div className="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+								<div className="mt-2 text-sm text-gray-600 dark:text-gray-400">
 									This starter kit is composed of{' '}
 									<a href="https://nextjs.org" className="underline" target="_blank" rel="noreferrer">
 										Next.js
@@ -139,9 +139,9 @@ const Home: FC = () => {
 				</div>
 
 				<div className="flex justify-center mt-4 sm:items-center sm:justify-between">
-					<div className="text-center text-sm text-gray-500 sm:text-left">
+					<div className="text-sm text-center text-gray-500 sm:text-left">
 						<div className="flex items-center">
-							<ShareIcon className="-mt-px w-5 h-5 text-gray-400" />
+							<ShareIcon className="w-5 h-5 -mt-px text-gray-400" />
 
 							<a href="https://twitter.com/m1guelpf" className="ml-1 underline">
 								Share
---
+++ src/pages/supa.tsx
@@ -0,0 +1,9 @@
+import { supabase } from '@/utils/supabase-client'
+import React from 'react'
+
+const Supabase = () => {
+	console.log(supabase)
+	return <div>Supabase</div>
+}
+
+export default Supabase
